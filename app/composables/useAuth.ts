21bd056c9b1bc9369ae6c1a309da455c6c38caa6
MohamedAbedy <hdjdrtrrf@gmail.com>
2025-09-19 17:48:19 +0300

    Edit auth

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -100,10 +100,7 @@ export const useAuth = () => {
           method: 'POST',
           body: {
             email,
-            clientUrl: [
-              'http://localhost:3000/reset-password',
-              'https://markly-realapi.netlify.app'
-            ]
+            clientUrl: 'https://markly-realapi.netlify.app'
           },
         }
       )
